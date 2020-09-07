63818c74de988c6886d0a95584f866e631a15925
João Victor Negreiros <65730630+joaovictornsv@users.noreply.github.com>
2020-09-07 20:24:13 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://jv-react-flix.herokuapp.com';
+  : 'https://reactflix-jv.herokuapp.com';
 
 export default {
   URL_BACKEND,
